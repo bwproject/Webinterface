68ef41b42c41533bab36578b0b954d8a2e5bcbd2
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2024-05-20 20:18:54 +0500

    Update constants.ts
    Signed-off-by: The_Mr_Mes109 <linkthemrmes109@gmail.com>

--- Frontend/src/lib/scripts/constants.ts
+++ Frontend/src/lib/scripts/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_PATH = import.meta.env.VITE_API_URL || "https://bot.projectbw.ru"
-export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://bot.projectbw.ru/invite"
+export const INVITE_URL = import.meta.env.VITE_INVITE_URL || "https://webbot.projectbw.ru/invite"
 
 export function get(path: string) {
 
